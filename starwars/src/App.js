f5cb71262635d0bcb8bde6995d36caf90e0f4a02
Your Name <you@example.com>
2019-08-31 11:52:51 -0500

    have data rendering

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Characters from "./Characters";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,11 +11,41 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
-    </div>
-  );
+  const [starWarsPeople, setStarWarsPeople] = useState ();
+
+  // function App() {
+  // const [nasaImage, setNasaImage] = useState();
+  // const [nasaTitle, setNasaTitle] = useState();
+  // const [nasaBody, setNasaBody] = useState();
+  // const [nasaMedia, setNasaMedia] = useState();
+
+const [ names, setNames ] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get('https://swapi.co/api/people/')
+			.then((res) => {
+				setNames(res.data.results);
+			})
+			.catch((error) => 
+      {console.log(error)
+      });
+	}, []);
+
+	return <div className="App">{names.map((name) => 
+  <Characters name={name.name} key={name.name} height={name.height}
+  mass={name.mass} hair_color={name.hair_color} />)}
+  
+  </div>;
 }
+
+
+//   return (
+//     <div className="App">
+//       <h1 className="Header">React Wars</h1>
+//      <Characters name={starWarsPeople}></Characters>
+//     </div>
+//   );
+// }
 
 export default App;
